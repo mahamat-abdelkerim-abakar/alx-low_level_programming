f9d030643d77d799e8238ec58bda2bbc0fd947c7
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-10 02:30:42 -0700

    cprogramming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * *main - Enrty point
+ * *
+ * *Return: Always 0
+ * */
+int main(void)
+{
+	printf(" Size of char: %zu byte(s)\n", sizeof(char));
+	printf(" Size of int: %zu byte(s)\n", sizeof(int));
+	printf(" Size of long int: %zu byte(s)\n", sizeof(long int));
+	printf(" Size of long long int: %zu byte(s)\n", sizeof(long long int));
+	printf(" Size of float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
