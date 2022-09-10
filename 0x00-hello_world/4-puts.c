89e170af9944e2b8e2fd0da9ae8f46dd2aafd1cb
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-10 02:26:20 -0700

    cprogramming4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * *main - Entry point
+ * *
+ * *Return: Always 0
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
