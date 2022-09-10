3b70fa9a87e866af85da93131387da4d2945777e
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-10 03:09:40 -0700

    cprogramming5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,19 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- * *main - Entry point
- * *
- * *Return: Always 0
+ *
+ * * main-Entry point
+ *
+ * * Return: 0 if successful
+ *
  * */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+int main(void)
+	
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+# include <stdio.h>
 /**
- * *main - Entry point
- * *
- * *Return: Always 0
- * */
-int main (void)
+ * main - Entry point
+ * My hello, print task
+ * Return: return 1 after program execution
+ */
+int main(void)
 {
-	printf(" with proper grammar, but the outcome is a piece of art, \n");
-
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
