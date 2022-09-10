0e4f8049652e58c6b8dba983b0499639bf6658f4
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-10 06:19:54 -0700

    cprogramming8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-nclude <stdio.h>
 #include <unistd.h>
 /**
  *  main - Entry point
- *  Return: Always 1 (Success)
+ *  Return: Always 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
