7cb3368b8aa43982903acc756fb9a9b5f577f9b7
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-16 08:41:20 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - function that checks for uppercase character.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int h = 1;
+
+	while (h <= 100)
+	{
+		if (h % 3 == 0 && h % 5 == 0);
+		printf("FizzBuzz ");
+		else if (h % 5 == 0)
+		{
+			if (h == 100)
+			{
+				printf("Buzz");
+				printf("\n");
+			}
+			else
+				printf("Buzz ");
+		}
+		else if (h % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", h);
+		h++;
+	}
+	return (0);
+}
