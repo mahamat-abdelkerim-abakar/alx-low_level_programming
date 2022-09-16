e882e29c05df90eecccb31be3a857592c9a46d79
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-16 09:39:45 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: random integer
+ *Return: void
+ */
+
+void print_number(int n)
+{
+	char ld, cr;
+	int r;
+	int ct = 0;
+
+	if (n < 0)
+	{
+		_putchar ('-');
+		ld = (char)('0' - (n % 10));
+		n = n / -10;
+	}
+
+	else
+	{
+		ld = (char)((n % 10) + '0');
+		n = n / 10;
+	}
+	
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n = n / 10;
+		ct++;
+	}
+
+	while (r > 0)
+	{
+		cr = (char)((r % 10) + '0');
+		_putchar(cr);
+		r = r / 10;
+		ct--;
+	}
+	while (ct != 0)
+	{
+		_putchar('0');
+		ct--;
+	}
+	_putchar(ld);
+}
