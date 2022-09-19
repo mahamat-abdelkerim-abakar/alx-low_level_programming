2c19ce243727d3e08d94e213f81f3bba2f8da0f7
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-19 11:52:11 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps two integers' values
+ *@a: first integer
+ *@b: second integer
+ *Return: returns nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
+
