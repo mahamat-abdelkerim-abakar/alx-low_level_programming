27b87792124afc42409284d4800a45c659a24527
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-20 03:01:35 -0700

    C - Pointers, arrays and strings
    C

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints a string
+ * @str: string to print
+ * Return: nothin
+ */
+
+void puts_half(char *str)
+{
+	int i, len = _strlen(str);
+
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
+		putchar(*(str + i));
+	putchar(*(str + i));
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: the length of the given string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+
