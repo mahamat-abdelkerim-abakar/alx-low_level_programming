c154d3b5a2ba8d19c189a44fb6d7f10e6e27adbe
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-22 03:51:01 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,24 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ * Return: address of s
  */
 
-char *cap_string(char *s)
+char *leet(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	for (i = 0; *(s + i); i++)
 	{
-		for (i = 0; i < 13; i++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
-		count++;
 	}
 	return (s);
 }
