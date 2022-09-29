99196404ef73708b0dcd5fd605839b0f608e3bd1
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-09-29 11:14:31 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@ int is_prime_number(int n)
 
 	if (n <= 1)
 		return (0);
-	return (is_prime(n, start));
+	return (is_prime_number(n, start));
 }
 
 /**
@@ -26,6 +26,6 @@ int is_prime(int n, int start)
 		return (1);
 	else if (n % start == 0)
 		eturn (0);
-	return (is_prime(n, start - 1));
+	return (is_prime_number(n, start - 1));
 }
 
