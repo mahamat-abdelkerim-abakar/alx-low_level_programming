d59b23b4651a965b822c13b2fb206de47859dbad
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-10-03 06:51:08 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num_1, num_2, mul;
+
+	if (argc != 3)
+		printf("Error\n");
+	else
+	{
+		num_1 = atoi(argv[1]);
+		num_2 = atoi(argv[2]);
+		mul = num_1 * num_2;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
