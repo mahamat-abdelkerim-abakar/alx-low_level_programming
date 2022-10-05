e3aaacaae323955f801b626b678c75664eb6a524
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-10-05 13:09:30 -0700

    libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -21,4 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* MAIN_H */
+#endif
