49e964f5dba9592b68b85a60008bdd11003db955
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-10-07 03:05:04 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,9 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
+	
 	block = malloc(sizeof(*block) * ((max - min) + 1));
+
 	if (block != NULL)
 	{
 		for (i = min; i <= max; i++)
