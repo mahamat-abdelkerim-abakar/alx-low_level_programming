1d4a18e1c1fe36de3a262e281027265949abc058
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-10-10 10:13:56 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include "dog.h"
+char *_strdup(char *str);
+
+
+
+/**
+ * new_dog - function with 3 arguments
+ * @name: char type pointer
+ * @age: float type
+ * @owner: char type pointer
+ * Description: creates a new dog
+ * Return: NULL if fail or pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
+		return (NULL);
+
+	new_d->name = _strdup(name);
+	if (!new_d->name)
+	{
+		free(new_d);
+		return (NULL);
+	}
+	new_d->age = age;
+	new_d->owner = _strdup(owner);
+	if (!new_d->owner)
+	{
+		free(new_d->name);
+		free(new_d);
+		return (NULL);
+	}
+
+	return (new_d);
+
+}
+
+/**
+ *_strdup - function with one argument
+ * @str: string argument
+ * Description: returns a pointer to allocated space in memory
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
