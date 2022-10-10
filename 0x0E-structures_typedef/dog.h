501c9382f7f2098c9333355b8538d1d8691dd982
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-10-10 09:46:11 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,21 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
 
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - Define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
 };
-
-/**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
