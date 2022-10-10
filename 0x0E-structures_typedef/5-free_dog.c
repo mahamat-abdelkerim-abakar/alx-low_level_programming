ded270aba8cbc0466b2417708e49cfa2d0c4798f
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-10-10 10:19:21 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - function with single argument
+ * @d: pointer type
+ * Description: frees dogs
+ * Return: na
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
