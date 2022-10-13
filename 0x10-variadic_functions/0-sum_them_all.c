92e3ecba03d552e277a14dd9d6cfe4fe5b49c5cd
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-10-13 07:42:34 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,6 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
-
 	va_list li;
 	int sum = 0;
 	unsigned int i;
@@ -26,7 +25,6 @@ int sum_them_all(const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 			sum += va_arg(li, int);
 	}
-
 
 	va_end(li);
 	return (sum);
