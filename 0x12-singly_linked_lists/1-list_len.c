6b1fa84c19ceec029d1887ee1751a28e11b8710e
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-10-21 13:02:45 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * list_len - finds the number of elements in a linked list
- *@h: pointer to the list
+ * list_len - counts the number of nodes in a linked list
+ * @h: head of the list
  *
- *Return: the number of elements
-*/
-
+ * Return: the number of elements
+ */
 size_t list_len(const list_t *h)
+{
+	const list_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
 	{
-		size_t elements = 0;
-
-		while (h)
-		{
-			elements++;
-			h = h->next;
-		}
-
-		return (elements);
+		count += 1;
+		cursor = cursor->next;
 	}
+	return (count);
+}
