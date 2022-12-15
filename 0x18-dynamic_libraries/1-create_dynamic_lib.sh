6cbe3db33686f8581e768c84dcf6423877ef5b88
Mahamat Abdelkerim Abakar <mahamatabdelkerim863@gmail.com>
2022-12-15 03:06:00 -0800

    task 2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
